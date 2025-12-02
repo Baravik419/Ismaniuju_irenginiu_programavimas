06c8cdc5d5c0fe62a0b52aaf2462c48a81280533
Baravik419 <ervin.borkovski@gmail.com>
2025-12-02 19:51:31 +0200

    Finished with 2LD

--- 2LD/src/main/java/com/example/a2ld/MainActivity.kt
+++ 2LD/src/main/java/com/example/a2ld/MainActivity.kt
@@ -1,47 +1,17 @@
 package com.example.a2ld
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.a2ld.ui.theme._1LDTheme
+import android.widget.Button // mygtukų biblioteka
+import android.widget.TextView // teksto biblioteka
+import android.util.Log //
+import android.view.View //
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : ComponentActivity() {
+    Button button1, button2, button3;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            _1LDTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    _1LDTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
